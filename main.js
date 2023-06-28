6c9fed62b086624af2d594b846c859e1b1ea8f9e
adrien <adrien@malingrey.fr>
2023-06-28 03:21:32 +0200

    less light

--- main.js
+++ main.js
@@ -82,6 +82,8 @@ scene.background = new THREE.CubeTextureLoader()
         'lf.jpg',
     ] );
 
+window.scene = scene
+
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.rotation.order = 'YXZ';
 
@@ -259,10 +261,10 @@ const sun = new THREE.Vector3();
 
 // Lights
 
-const ambientLight = new THREE.AmbientLight(0x404040, 9);
+const ambientLight = new THREE.AmbientLight(0x404040, 7);
 scene.add(ambientLight);
 
-const sunLight = new THREE.DirectionalLight(0xfffae8, 0.7);
+const sunLight = new THREE.DirectionalLight(0xfffae8, 0.5);
 sunLight.castShadow            = true;
 sunLight.shadow.camera.near    = 0.1;
 sunLight.shadow.camera.far     =  1.4 * mazeWidth;
